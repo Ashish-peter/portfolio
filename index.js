3eb7e75fe8f6fe083ae1959e654348be60319d88
Yajnesh Kumar <yajnesh2401@gmail.com>
2021-03-20 13:38:01 +0530

    Registration form Spreadsheet activity added by yajnesh

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 //alert("hello");
+
+//##############################################################Registration Form JS Code #########################################################
+
+const scriptURL ='https://script.google.com/macros/s/AKfycbwfQOoUuMG8UfqRjd4AZkSSLCQwKUE1v92czC0MRGxKe0l-hqnwTAwDptwolrukfw52/exec';
+const form = document.forms['Registration Form Details'];
+
+form.addEventListener('submit',e =>{
+  e.preventDefault()
+  fetch(scriptURL,{method:'POST', body : new FormData(form)}).then(
+    response=> alert("Thanks for Registering with us. We will contact you soon")
+  )
+  .catch(error=> console.error('Error!', error.message))
+})
