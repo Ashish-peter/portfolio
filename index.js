886b20ebae6d854bcf7a68b7e0ff42b1377669d5
Yajnesh Kumar <yajnesh2401@gmail.com>
2021-03-21 22:27:28 +0530

    few changes added by yajnesh

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const scriptURL ='https://script.google.com/macros/s/AKfycbwfQOoUuMG8UfqRjd4AZkS
 const form = document.forms['Registration Form Details'];
 
 form.addEventListener('submit',e =>{
-  e.preventDefault()
-  fetch(scriptURL,{method:'POST', body : new FormData(form)}).then(
-    response=> alert("Thanks for Registering with us. We will contact you soon")
-  )
-  .catch(error=> console.error('Error!', error.message))
+  var result=1;
+  if(result===2){
+    e.preventDefault()
+    fetch(scriptURL,{method:'POST', body : new FormData(form)}).then(
+      response=> alert("Thanks for Registering with us. We will contact you soon")
+    )
+    .catch(error=> console.error('Error!', error.message))
+  }
+  else{
+    console.log("You already submitted form");
+  }
+
 })
