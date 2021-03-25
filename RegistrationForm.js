c380dbe5c8e5670b4da4f0cd5561ccf441b1a442
Ashish-peter <2016ashishk@gmail.com>
2021-03-25 21:29:04 +0530

    update registration.js post method

--- RegistrationForm.js
+++ RegistrationForm.js
@@ -13,76 +13,55 @@ function getdata() {
     return res.json()
   }).then((json) => {
 
-    //fetching length of contact_number in the total list
-    var count = 0; //global var
-    json.data.forEach((ele => {
-      count++;
-    }))
 
 
-    //the below code will execute on clicking submit buttom
+
+
     form.addEventListener('submit', e => {
-      var userContactNumber = document.getElementById("inputContactNo").value;
-      // console.log(x);
 
-      var result1 = "";
-      var result2 = "";
-      for (var i = 0; i < count; i++) {
-        if (userContactNumber == json.data[i].contact_number) {
-          result1 = userContactNumber;
-          result2 = json.data[i].contact_number;
-          break;
-        }
-      }
+      e.preventDefault()
+      fetch(scriptURL, {
+          method: 'POST',
+          body: new FormData(form)
+        }).then(function() {
 
 
-      if (result1 == result2) {
+          var userContactNumber = document.getElementById("inputContactNo").value;
+          // console.log(userContactNumber);
 
-        console.log("you arleady");
-
-
-        //console.log('you already participated');
-        // e.preventDefault()
-        // fetch(scriptURL, {
-        //     method: 'POST',
-        //     //body: new FormData(form)
-        //   }).then(
-        //     response => alert("you already participated")
-        //   )
-          //.catch(error => console.error('Error!', error.message))
-        }
-
-        e.preventDefault()
-        fetch(scriptURL, {
-            method: 'POST',
-            body: new FormData(form)
-          }).then(
-            response => alert("Thanks for Registering with us. We will contact you soon")
-          )
-          .catch(error => console.error('Error!', error.message))
+          //fetching length of contact_number in the total list
+          var count = 0; //global var
+          json.data.forEach((ele => {
+            count++;
+          }))
+          // console.log(count);
+          var result1 = "";
+            var result2 = "";
+            for (var i = 0; i < count; i++) {
+              if (userContactNumber == json.data[i].contact_number) {
+                result1 = userContactNumber;
+                result2 = json.data[i].contact_number;
+                alert("you already singup");
+                break;
+              }
+            }
+            // console.log(result1);
+            // console.log(result2);
 
 
 
-
-
-
-
-
-
-
+        })
+        .catch(error => console.error('Error!', error.message)).then(function(){
+          console.log("it's work");
+        })
 
 
     })
-    // console.log(json.data[0].contact_number);
 
   })
+
+
 }
-
-
-
-console.log("hello");
-
-
 
 
 // form.addEventListener('submit',e =>{
